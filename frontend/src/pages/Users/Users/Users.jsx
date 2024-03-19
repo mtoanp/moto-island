ea7706f8d700e39ada27ace1f9ff65234a508c5d
mToan <mtoanp@outlook.com>
2024-03-19 11:46:24 +0100

    frontend | setup all links

---
+++ frontend/src/pages/Users/Users/Users.jsx
@@ -0,0 +1,9 @@
+import "./Users.scss";
+
+export default function Users() {
+  return (
+    <div className="users">
+      <h1 className="text-3xl font-bold text-red-300">Users list</h1>
+    </div>
+  );
+}
