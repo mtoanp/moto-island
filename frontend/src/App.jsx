a49c74f4212496dd5c52a293d86959bff3480112
MarCodeQ <marcus.4vu@gmail.com>
2024-03-21 13:54:45 +0700

    frontend | remake homepage and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,19 @@ import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App font-[Poppins] h-screen">
+			<link
+				href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+				rel="stylesheet"
+			></link>
+			<NavBar />
+			<main>
+				<Outlet />
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
