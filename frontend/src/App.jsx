6234668a35e8ddd2511560411bf1e2e2eb34413b
mToan <mtoanp@outlook.com>
2024-03-21 13:27:31 +0100

    fontend | reset NavBar filename

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.scss";
 import { Outlet } from "react-router-dom";
-import NavBar from "./components/Navbar/NavBar";
+import NavBar from "./components/Navbar2/NavBar";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-	return (
-		<div className="App">
-			<NavBar />
-			<main>
-				<Outlet />
-			</main>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
