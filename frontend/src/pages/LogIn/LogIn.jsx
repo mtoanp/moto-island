ac17e20711b3c89ea55fd99d54ca8b3b7dca3bcf
MarCodeQ <marcus.4vu@gmail.com>
2024-03-24 17:38:53 +0700

    frontend | remake login & register form

--- frontend/src/pages/LogIn/LogIn.jsx
+++ frontend/src/pages/LogIn/LogIn.jsx
@@ -1,60 +1,52 @@
 import "./LogIn.scss";
 import { useAuth } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
-
 import purpleSky from "../../assets/purpleSky.jpg";
+
 export default function LogIn() {
-  const { authLogIn } = useAuth();
-  const navigate = useNavigate();
+	const { authLogIn } = useAuth();
+	const navigate = useNavigate();
+	const LogInAs = (acc) => {
+		authLogIn(acc);
+		navigate("/");
+	};
 
-  const LogInAs = (acc) => {
-    authLogIn(acc);
-    navigate("/");
-  };
-
-  return (
-    <section className="main">
-      <img src={purpleSky} alt="..." className="login-bg" />
-      <div className="login-page">
-        <div className="auth-container text-center">
-          <button className="btn btn-primary " onClick={() => LogInAs("user")}>
-            US
-          </button>
-          <button
-            className="btn btn-primary ml-4"
-            onClick={() => LogInAs("admin")}>
-            AD
-          </button>
-        </div>
-        <div className="login-form">
-          <h1 className="login">Login</h1>
-          <form>
-            <div className="input-box">
-              <input
-                type="text"
-                placeholder="Username"
-                id="username"
-                name="username"
-                required
-              />
-              <i className="bx bxs-user"></i>
-            </div>
-            <div className="input-box">
-              <input
-                type="password"
-                placeholder="Password"
-                id="password"
-                name="password"
-                required
-              />
-              <i className="bx bxs-lock-alt"></i>
-            </div>
-            <button type="submit" className="login-button">
-              Login
-            </button>
-          </form>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="big-cover">
+			<img src={purpleSky} alt="..." className="login-bg" />
+			<div className="login-page">
+				<div className="auth-container text-center">
+					<button className="btn btn-primary " onClick={() => LogInAs("user")}>
+						US
+					</button>
+					<button
+						className="btn btn-primary ml-4"
+						onClick={() => LogInAs("admin")}
+					>
+						AD
+					</button>
+				</div>
+				<div className="login-form">
+					<h1 className="login-title">Login</h1>
+					<div className="input-box">
+						<input type="text" id="username" required="required" />
+						<label className="user-label" htmlFor="">
+							Username
+						</label>
+						<i className="bx bxs-user"></i>
+					</div>
+					<div className="input-box">
+						<input type="password" id="password" required="required" />
+						<label className="pass-label" htmlFor="">
+							Password
+						</label>
+						<i className="bx bxs-lock-alt"></i>
+					</div>
+					<button type="submit" className="login-button">
+						Login
+					</button>
+				</div>
+			</div>
+		</section>
+	);
 }
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -4,46 +4,35 @@ import purpleSky from "../../assets/purpleSky.jpg";
 
 export default function Register() {
 	return (
-		<section className="main">
+		<section className="big-cover">
 			<img src={purpleSky} alt="..." className="login-bg" />
 			<div className="login-page">
 				<div className="login-form">
-					<h1 className="login">Register</h1>
-					<form>
-						<div className="input-box">
-							<input
-								type="text"
-								placeholder="Username"
-								id="username"
-								name="username"
-								required
-							/>
-							<i className="bx bxs-user"></i>
-						</div>
-						<div className="input-box">
-							<input
-								type="password"
-								placeholder="Password"
-								id="password"
-								name="password"
-								required
-							/>
-							<i className="bx bxs-lock-alt"></i>
-						</div>
-						<div className="input-box">
-							<input
-								type="password"
-								placeholder="Confirm Password"
-								id="confirm-password"
-								name="confirm-password"
-								required
-							/>
-							<i className="bx bxs-lock-alt"></i>
-						</div>
-						<button type="submit" className="login-button">
-							<Link to="/error">Create</Link>
-						</button>
-					</form>
+					<h1 className="login-title">Register</h1>
+					<div className="input-box">
+						<input type="text" id="username" required="required" />
+						<label className="user-label" htmlFor="">
+							Email
+						</label>
+						<i className="bx bxs-user"></i>
+					</div>
+					<div className="input-box">
+						<input type="password" id="password" required="required" />
+						<label className="pass-label" htmlFor="">
+							Password
+						</label>
+						<i className="bx bxs-lock-alt"></i>
+					</div>
+					<div className="input-box">
+						<input type="password" id="password" required="required" />
+						<label className="pass-label" htmlFor="">
+							Confirm Password
+						</label>
+						<i className="bx bxs-lock-alt"></i>
+					</div>
+					<button type="submit" className="login-button">
+						<Link to="/error">Create</Link>
+					</button>
 				</div>
 			</div>
 		</section>
