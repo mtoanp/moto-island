7730e243910230045d94c740dbeb94ad5448c775
mToan <mtoanp@outlook.com>
2024-03-24 17:22:33 +0100

    frontend | form : somes style

--- frontend/src/pages/LogIn/LogIn.jsx
+++ frontend/src/pages/LogIn/LogIn.jsx
@@ -4,49 +4,53 @@ import { useNavigate } from "react-router-dom";
 import purpleSky from "../../assets/purpleSky.jpg";
 
 export default function LogIn() {
-	const { authLogIn } = useAuth();
-	const navigate = useNavigate();
-	const LogInAs = (acc) => {
-		authLogIn(acc);
-		navigate("/");
-	};
+  const { authLogIn } = useAuth();
+  const navigate = useNavigate();
+  const LogInAs = (acc) => {
+    authLogIn(acc);
+    navigate("/");
+  };
 
-	return (
-		<section className="big-cover">
-			<img src={purpleSky} alt="..." className="login-bg" />
-			<div className="login-page">
-				<div className="auth-container text-center">
-					<button className="btn btn-primary " onClick={() => LogInAs("user")}>
-						US
-					</button>
-					<button
-						className="btn btn-primary ml-4"
-						onClick={() => LogInAs("admin")}
-					>
-						AD
-					</button>
-				</div>
-				<div className="login-form">
-					<h1 className="login-title">Login</h1>
-					<div className="input-box">
-						<input type="text" id="username" required="required" />
-						<label className="user-label" htmlFor="">
-							Username
-						</label>
-						<i className="bx bxs-user"></i>
-					</div>
-					<div className="input-box">
-						<input type="password" id="password" required="required" />
-						<label className="pass-label" htmlFor="">
-							Password
-						</label>
-						<i className="bx bxs-lock-alt"></i>
-					</div>
-					<button type="submit" className="login-button">
-						Login
-					</button>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className="big-cover">
+      <img src={purpleSky} alt="..." className="login-bg" />
+      <div className="login-page">
+        <div className="auth-container text-center">
+          <button className="btn btn-primary " onClick={() => LogInAs("user")}>
+            US
+          </button>
+          <button
+            className="btn btn-primary ml-4"
+            onClick={() => LogInAs("admin")}>
+            AD
+          </button>
+        </div>
+        <div className="login-form">
+          <h1 className="login-title">Login</h1>
+          <div className="input-box">
+            <input
+              type="text"
+              id="username"
+              required="required"
+              autoComplete="off"
+            />
+            <label className="user-label" htmlFor="">
+              Username
+            </label>
+            <i className="bx bxs-user"></i>
+          </div>
+          <div className="input-box">
+            <input type="password" id="password" required="required" />
+            <label className="pass-label" htmlFor="">
+              Password
+            </label>
+            <i className="bx bxs-lock-alt"></i>
+          </div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
