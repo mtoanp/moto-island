f56a845afdeaad300c968a389d5bf0dd3ed7c33d
MarCodeQ <marcus.4vu@gmail.com>
2024-03-26 17:53:18 +0700

    frontend | history

--- frontend/src/pages/Orders/NewOrder/NewOrder.jsx
+++ frontend/src/pages/Orders/NewOrder/NewOrder.jsx
@@ -4,7 +4,7 @@ import mansky from "../../../assets/mansky.jpg";
 
 export default function NewOrder() {
 	return (
-		<div className="biggest-stage ">
+		<div className="biggest-stage">
 			<img src={mansky} alt="..." className="background-stage" />
 			<div className="second-stage">
 				<div className="title-stage">New Order</div>
