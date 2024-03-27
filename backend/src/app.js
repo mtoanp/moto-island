6d591885098b6a273fb79664d29a488dc3c88353
mToan <mtoanp@outlook.com>
2024-03-27 12:50:14 +0100

    backend | add router base structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,4 +6,11 @@ const logger = require("morgan");
 const app = express();
 app.use(logger("dev")).use(express.json()); // Parse JSON bodies
 
+// ROUTER
+const router = require("./routes/router");
+const api = require("./routes/api");
+app.use("/", router);
+app.use("/api", api); // Use the routes defined in the router file with the /api prefix
+
+// Export the Express application
 module.exports = app;
